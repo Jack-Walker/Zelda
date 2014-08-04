5190bd45ea42ecdaa00b83425de6dddff20ad54c
Jack Walker <jackwalker5421@gmail.com>
2014-08-04 12:18:15 -0400

    no message

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+    private Player player;
+
+	// Use this for initialization
+	void Start () 
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = player.transform.position + (transform.forward * -4.0f) + (transform.up * 2.0f) + (transform.right * 2.0f);
+        
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+    public UnityEngine.Camera mainCamera;
+	// Use this for initialization
+	void Start () 
+    {
+        mainCamera = GameObject.Find("PositionCam/DefaultCam").camera;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
